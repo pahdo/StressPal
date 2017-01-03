968bfea3a91f9c1df32116e9a15676bd7932dce7
Daniel Zou <danielwzou@gmail.com>
2017-01-03 02:46:00 -0600

    Removed tellWithCard and fixed SSML on one fact.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ var languageStrings = {
                 "Focus on your breath. Slowly breathe in and out for the next fifteen seconds. Count in your head, and don't let your mind wander.",
                 "Tense your body. <break time=\"1s\"/> Release your body. <break time=\"1s\"/> Tense your body. <break time=\"1s\"/> Release your body. <break time=\"1s\"/> Repeat this 8 more times.",
                 "Imagine yourself walking on the beachside under a bright blue sky. <audio src=\"https://s3.amazonaws.com/hack-the-dorm/beach_vibes_short_48.mp3\"/>",
-                "Imagine yourself in a field of flowers, skipping from place to place. <audio src=\https://s3.amazonaws.com/hack-the-dorm/light_orchestra_short_48.mp3\"/>"
+                "Imagine yourself in a field of flowers, skipping from place to place. <audio src=\"https://s3.amazonaws.com/hack-the-dorm/light_orchestra_short_48.mp3\"/>"
             ],
             "SKILL_NAME" : "StressPal",
             "GET_FACT_MESSAGE" : "Close your eyes. Breathe in. <break time=\"2s\"/> Breathe out. <break time=\"2s\"/>",
@@ -43,7 +43,7 @@ var handlers = {
         var randomFact = factArr[factIndex];
 
         var speechOutput = this.t("GET_FACT_MESSAGE") + randomFact;
-        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact)
+        this.emit(':tell', speechOutput, this.t("SKILL_NAME"), randomFact)
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = this.t("HELP_MESSAGE");
